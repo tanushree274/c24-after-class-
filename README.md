06b77889b131bcc8558d028c84d232497416f6b9
tanushree274 <71226395+tanushree274@users.noreply.github.com>
2021-07-06 21:46:25 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MatterJSBoilerPlate
-MatterJSBoilerPlate
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -1,59 +1,9 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-var paper1;
-var ground1;
-
-function preload()
-{
-	
-}
-
 function setup() {
-	createCanvas(800, 400);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-paper1= new Paper (50,100,20);
-ground1 = new Ground (400,380,800,20);
-
-binSprite1=createSprite(width/2.6 ,310,20,100);
-	binSprite2=createSprite(width/2.2,365,130,10);
-	binSprite3=createSprite(width/1.9,310,20,100);
-binSprite1.shapeColor=color(255,0,0);
-binSprite2.shapeColor="red";
-binSprite3.shapeColor="red";
-
-	Engine.run(engine);
-
-
-  
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
 }
-
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  paper1.display();
-  ground1.display();
+  background(255,255,255);  
   drawSprites();
- 
 }
-
-function keyPressed(){
-
-	if (keyCode===UP_ARROW ){
-Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:85});
-	}
-}
-
-
-
-
